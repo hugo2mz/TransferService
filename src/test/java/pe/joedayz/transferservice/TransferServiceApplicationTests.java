c048e5dc6560c455fb67779dd6152605d00637a6
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-20 22:07:44 -0500

    Transferservice

---
+++ src/test/java/pe/joedayz/transferservice/TransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
